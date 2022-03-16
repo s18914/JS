c07499988b2b7d8ab019a0fc29bd3082f995118c
s18914 <s18914@pjwstk.edu.pl>
2022-03-16 16:54:49 +0100

    finish task 2

--- JS2/js2.2/main.js
+++ JS2/js2.2/main.js
@@ -1,8 +1,38 @@
 const el = document.createElement("div");
 document.body.appendChild(el);
+document.body.style.height = "100000px";
 
-let height = 0;
+let size = 0;
+let isGrow = 1;
 
-window.addEventListener("scroll", function(){
-    
-})
+//style
+el.style.width = "100%";
+el.style.position = "fixed";
+el.style.left = el.style.top = "0";
+el.style.backgroundColor = "green";
+el.style.height = size + "px";
+
+const changeHeight = function(){
+
+    if(isGrow){
+        size+=4;
+        el.style.height = size + "px";
+    }
+    else if(!isGrow){
+        size-=4;
+        el.style.height = size + "px";
+    }
+
+  if(size >= window.innerHeight/2 || size == 0){
+    if(isGrow) {
+        el.style.backgroundColor = "red";
+    }
+    else {
+        el.style.backgroundColor = "green";
+    }
+    isGrow = !isGrow;
+  }
+};
+
+
+window.addEventListener("scroll", changeHeight);
