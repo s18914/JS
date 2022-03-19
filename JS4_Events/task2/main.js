a8c5d234c5d71c50408efe968db0540e2e5f4783
s18914 <s18914@pjwstk.edu.pl>
2022-03-19 10:16:37 +0100

    task 2 complete

---
+++ JS4_Events/task2/main.js
@@ -0,0 +1,20 @@
+document.body.addEventListener("click", function(e) {
+    const x = e.clientX;
+    const y = e.clientY;
+    console.log(x + " " + y);
+
+    if(x%2 == 0 && y%2 == 0){
+        document.body.style.backgroundColor = "red";
+    }
+    else if(x%2 == 1 && y%2 == 1) {
+        document.body.style.backgroundColor = "blue";
+    }
+    else {
+        document.body.style.backgroundColor = "green";
+    }
+})
+// KOLOR GENEROWANY W ZALEŻNOŚCI OD TEGO GDZIE KLIKNIEMY
+
+// jeśli wartość dla x (event.clientX) i y (event.clientY) jest parzysta (np. 100,122)  to kolor czerwony
+// jesli wartość dla x i y jest nieparzysta to kolor niebieski
+// jeśli jeden z kolorów jest parzysty a drugi nieparzysty to kolor zielony
